26087b973d5d992a50fff3c3e99ddcd2c374a01b
Shubhankar Oad <shubhankar.oad.cse20@itbhu.ac.in>
2022-11-01 00:54:47 +0530

    NavBar initial

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,32 +1,60 @@
 <template>
   <div id="app">
-    <section class="navigation">
-      <div class="nav-container">
-        <div class="brand">
-          <a href="#">
-            <h1>Le Lotus Grand</h1>
-          </a>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="name" href="/">Le Lotus Grand</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
 
-        <nav>
-          <div class="nav-mobile">
-            <a id="nav-toggle" href="#"><span /></a>
-          </div>
-
-          <div class="menu-main-menu-container">
-            <ul class="nav-list">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Booking</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li>
-                <a href="#">Login/Register</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+      <div id="navbarSupportedContent" class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          <!-- <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about">Link</a>
+          </li> -->
+          <!-- <li class="nav-item dropdown">
+            <a
+              id="navbarDropdown"
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a> -->
+          <!-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider" />
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div> -->
+          <!-- </li> -->
+          <!-- <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li> -->
+        </ul>
+        <a class="nav-link my-2 my-lg-0" href="/">Home
+        </a>
+        <a class=" nav-link my-2 my-lg-0" href="about">About
+        </a>
+        <a class=" nav-link my-2 my-lg-0" href="#">Profile
+        </a>
+        <a class=" nav-link my-2 my-lg-0" href="#">Login/Logout
+        </a>
       </div>
-    </section>
+    </nav>
   </div>
 </template>
 
