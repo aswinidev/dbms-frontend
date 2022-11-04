cc328b1beeb1d8c74bf9cdcc16d0a9a9f3e2aba3
gargig02 <gargig02@gmail.com>
2022-11-04 23:19:31 +0530

    edit profile and booking

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <DashboardAswini />
+    <DashboardGargi />
   </div>
 </template>
---
+++ pages/editProfile.vue
@@ -0,0 +1,241 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="bgImg" />
+    <div class="content">
+      <div class="d-flex h-100 align-items-center justify-content-center">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7 col-sm-6 ml-auto mr-auto">
+              <div class="card">
+                <div class="card-body">
+                  <form>
+                    <div class="glowBox">
+                      <h2 class="title">
+                        Edit Profile
+                      </h2>
+                    </div>
+                  </form>
+                  <div class="signUp">
+                    <span class="req">
+                      <input
+                        v-model="fname"
+                        type="text"
+                        class="form"
+                        placeholder="First Name"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="lname"
+                        type="text"
+                        class="form"
+                        placeholder="Last Name"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="pEmail"
+                        type="text"
+                        class="form"
+                        placeholder="Email"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="pswd"
+                        type="password"
+                        class="form"
+                        placeholder="Password"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="houseNo"
+                        type="text"
+                        class="form"
+                        placeholder="House Number"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="city"
+                        type="text"
+                        class="form"
+                        placeholder="City"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="state"
+                        type="text"
+                        class="form"
+                        placeholder="State"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="country"
+                        type="text"
+                        class="form"
+                        placeholder="Country"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="pinCode"
+                        type="text"
+                        class="form"
+                        placeholder="Pincode"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="gender"
+                        type="text"
+                        class="form"
+                        placeholder="Gender"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="alternateEmailAddress"
+                        type="text"
+                        class="form"
+                        placeholder="Alternate Email Address"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                    <span>
+                      <input
+                        v-model="aadharCardNumber"
+                        type="text"
+                        class="form"
+                        placeholder="Aadhar Card Number"
+                        autocomplete="off"
+                        style="cursor: auto;"
+                      >
+                      <span class="underline" />
+                    </span>
+                  </div>
+                  <center>
+                    <button class="custom-btn btn-1" @click="register">
+                      <span>Edit</span>
+                    </button>
+                  </center>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const myaxios = axios.create({ baseURL: 'http://localhost:8080' })
+export default {
+  data () {
+    return {
+      fname: '',
+      lname: '',
+      pEmail: '',
+      pswd: '',
+      houseNo: '',
+      state: '',
+      city: '',
+      country: '',
+      pinCode: '',
+      gender: '',
+      alternateEmailAddress: '',
+      aadharCardNumber: ''
+
+    }
+  },
+  mounted () {
+    myaxios
+      .get(
+        '/edit-profile'
+        // get mapping for userCustomer
+      )
+      .then((response) => {
+        this.fname = response.data.fname
+        this.lname = response.data.lname
+        this.pEmail = response.data.pEmail
+        this.pswd = response.data.pswd
+        this.houseNo = response.data.houseNo
+        this.state = response.data.state
+        this.city = response.data.city
+        this.country = response.data.country
+        this.pinCode = response.data.pinCode
+        this.gender = response.data.gender
+        this.alternateEmailAddress = response.data.alternateEmailAddress
+        this.aadharCardNumber = response.data.aadharCardNumber
+      }
+      )
+      .catch((error) => {
+        this.errorMessage = error.message
+        console.error('There was an error!', error)
+      })
+  },
+  methods: {
+    register () {
+      myaxios.post('/edit-profile', {
+        fname: this.fname,
+        lname: this.lname,
+        pEmail: this.pEmail,
+        pswd: this.pswd,
+        houseNo: this.houseNo,
+        state: this.state,
+        city: this.city,
+        country: this.country,
+        pinCode: this.pinCode,
+        gender: this.gender,
+        alternateEmailAddress: this.alternateEmailAddress,
+        aadharCardNumber: this.aadharCardNumber
+      }).then(function (response) {
+        console.log(response.data)
+      })
+    }
+  }
+}
+</script>
+
+  <style scoped>
+  @import '@/assets/register/register.css';
+  </style>
