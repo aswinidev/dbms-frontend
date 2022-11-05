2bf796b94bab62e03761cba81f11501164c02b43
Shubhankar Oad <shubhankar.oad.cse20@itbhu.ac.in>
2022-11-06 01:32:25 +0530

    Employee related

---
+++ components/EmployeeList.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <div class="container">
+      <h1 style="text-align: center; margin-bottom: 40px;">
+        Employee List
+      </h1>
+      <div class="request-details">
+        <div class="company">
+          <div class="placeholder">
+            <i class="fas fa-building" />
+            <p>Employee ID</p>
+          </div>
+          <h3>{{ bookingid }}</h3>
+        </div>
+        <div class="company">
+          <div class="placeholder">
+            <i class="fas fa-building" />
+            <p>First Name</p>
+          </div>
+          <h3>{{ checkin }}</h3>
+        </div>
+        <div class="company">
+          <div class="placeholder">
+            <i class="fas fa-building" />
+            <p>Last Name</p>
+          </div>
+          <h3>{{ checkout }}</h3>
+        </div>
+        <div class="company">
+          <div class="placeholder">
+            <i class="fas fa-building" />
+            <p>Department Name</p>
+          </div>
+          <h3>{{ checkout }}</h3>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      props: ['bookingid', 'checkin', 'checkout']
+    }
+  },
+  methods: {
+    goToDetails (event) {
+      this.$emit('clicked')
+    }
+  }
+}
+</script>
+
+  <style scoped>
+  @import '@/assets/booking/bookingList.css'
+  </style>
--- pages/admin/employee.vue
+++ pages/admin/employee.vue
@@ -1,5 +1,60 @@
 <template>
   <div>
-    Employee page
+    <NavBar />
+    <div v-if="getDetails==false">
+      <!-- <div class="page">
+        <div class="buttons">
+          <a class="btn add-new" href="/booking-form">Add New Booking</a>
+        </div>
+      </div> -->
+      <EmployeeList />
+      <EmployeeList
+        v-for="employee in employees"
+        :key="employee.employeeID"
+        :employeeid="employee.employeeID"
+        :checkin="booking.checkInDate"
+        :checkout="booking.checkOutDate"
+        @clicked="onClickListItem(booking.bookingID)"
+      />
+    </div>
+    <div v-else>
+      <button class="custom-btn btn-1" @click="goBacktoPage">
+        <span>Back</span>
+      </button>
+      <EmployeeDetails :bookingid="id" />
+    </div>
+
+    <Footer />
   </div>
 </template>
+
+<script>
+import EmployeeList from '~/components/EmployeeList.vue'
+
+export default {
+  components: { EmployeeList },
+  data () {
+    return {
+      getDetails: false,
+      employees: [],
+      id: ''
+    }
+  },
+  mounted () {
+    // getmapping
+  },
+  methods: {
+    onClickListItem (value) {
+      this.getDetails = true
+      this.id = value
+    },
+    goBacktoPage () {
+      this.getDetails = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+    @import '@/assets/booking/bookingList.css'
+</style>
