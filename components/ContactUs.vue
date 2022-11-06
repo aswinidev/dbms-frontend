752522069902d58e9173fa31c67559f672704ca7
P Aswini Kumar <aswini123p@gmail.com>
2022-11-06 15:13:47 +0530

    contactus

--- components/ContactUs.vue
+++ components/ContactUs.vue
@@ -5,25 +5,66 @@
         <span>Contact Us</span>
       </div>
       <div class="form-line">
-        <input type="text" class="form-input" required>
-        <label style="font-size: 18px;">Name</label>
+        <label style="font-size: 18px;">Name</label><br><br>
+        <input v-model="name" type="text" class="form-input" required>
       </div>
       <div class="form-line">
-        <input type="" class="form-input" required>
-        <label style="font-size: 18px;">Contact Number</label>
+        <label style="font-size: 18px;">Contact Number</label><br><br>
+        <input v-model="contactNumber" type="" class="form-input" required>
       </div>
       <div class="form-line">
-        <input type="Email" class="form-input" required>
-        <label style="font-size: 18px;">Email</label>
+        <label style="font-size: 18px;">Email</label><br><br>
+        <input v-model="emailID" type="Email" class="form-input" required>
       </div>
       <div class="form-line">
-        <textarea class="form-input" required rows="10" />
-        <label style="font-size: 18px;">Query</label>
+        <label style="font-size: 18px;">Query</label><br><br>
+        <textarea v-model="query" class="form-input" required rows="10" />
       </div>
-      <input type="button" class="form-button" value="Submit" style="font-size: 18px;">
+      <input type="button" class="form-button" value="Submit" style="font-size: 18px;" @click="sendQuery">
     </form>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+const myaxios = axios.create({ baseURL: 'http://localhost:8080' })
+export default {
+  data () {
+    return {
+      name: '',
+      contactNumber: '',
+      query: '',
+      emailID: ''
+    }
+  },
+  methods: {
+    sendQuery () {
+      myaxios
+        .post('/contactus',
+          {
+            name: this.name,
+            contactNumber: this.contactNumber,
+            query: this.query,
+            emailID: this.emailID
+          },
+          {
+            headers: {
+            // Authorization: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJMS05GTE4iLCJpYXQiOjE2NjcwNDkwNTQsImV4cCI6MTY2NzA1OTg1NH0.GdsK7YclD7Eeg6UJU2h8femd4FvPe1TOl8zbwm6iNd_gZejtH45Mo1YP8XIzdDrKbVA_7YshzZKHcbr3Dbw_1Q'
+              Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+          }
+        )
+        .then(function (response) {
+          console.log(response.data)
+        })
+        .catch((error) => {
+          this.errorMessage = error.message
+          console.error('There was an error!', error)
+        })
+    }
+  }
+}
+</script>
 
 <style scoped>
   * {
